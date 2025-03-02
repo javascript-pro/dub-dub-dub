7d9b0286c44d8b2ec0a657768145bbab70aeb796
Goldlabel pr0 <github@goldlabel.pro>
2025-03-02 09:55:11 +0000

    fixed build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,9 @@
-import styles from "./theme/page.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import Hero from "@repo/ui/Hero/Hero";
-import ResizingGrid from "@repo/ui/ResizingGrid/ResizingGrid";
+// import styles from "./theme/page.module.css";
+// import Image from "next/image";
+// import Link from "next/link";
+// import Hero from "@repo/ui/Hero/Hero";
+// import ResizingGrid from "@repo/ui/ResizingGrid/ResizingGrid";
 import Appshell from "@repo/ui/Appshell/Appshell";
-import chapterList from "../public/json/chapters.json";
 
 export default function Home() {
   return <Appshell />
--- apps/web/app/pageSSR.tsx
+++ apps/web/app/pageSSR.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Hero from "@repo/ui/Hero/Hero";
 import ResizingGrid from "@repo/ui/ResizingGrid/ResizingGrid";
 import Appshell from "@repo/ui/Appshell/Appshell";
-import chapterList from "../public/json/chapters.json";
+// import chapterList from "../public/json/chapters.json";
 
 export default function Home() {
   return <Appshell>
